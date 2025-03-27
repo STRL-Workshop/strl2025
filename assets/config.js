207e6a45868a277494983abab0a43b191baccf03
Zhiguo Long <alongtusuo@gmail.com>
2025-03-27 10:03:30 +0800

    initial commit

---
+++ assets/config.js
@@ -0,0 +1,7 @@
+(function() {
+  window._POSTHOG_REMOTE_CONFIG = window._POSTHOG_REMOTE_CONFIG || {};
+  window._POSTHOG_REMOTE_CONFIG['phc_qebaM3TPefX8DlNfehJq0ZGbE1PHUSg4KlxTlQ0dUye'] = {
+    config: {"token": "phc_qebaM3TPefX8DlNfehJq0ZGbE1PHUSg4KlxTlQ0dUye", "supportedCompression": ["gzip", "gzip-js"], "hasFeatureFlags": false, "captureDeadClicks": false, "capturePerformance": false, "autocapture_opt_out": false, "autocaptureExceptions": false, "analytics": {"endpoint": "/i/v0/e/"}, "elementsChainAsString": true, "sessionRecording": false, "heatmaps": false, "surveys": [], "defaultIdentifiedOnly": true},
+    siteApps: []
+  }
+})();
